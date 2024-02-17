e87e401cf599086d8b800bebcd51ec7dd49783e0
kimsieun99 <dotoryeop@gmail.com>
2024-02-17 23:59:45 +0900

    navbar구현중..

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div>헤</div>
-      <h1>아</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<div style={{height:'200vh'}}></div>} />
+        <Route path="/qna" element={<div style={{height:'200vh'}}>qna page</div>} />
+      </Routes>
     </>
   );
 }
